dc39aaff056b8e73e8ecc95f4c6ff7190f141b91
bessemouelhi <bessem.mouelhi@outlook.fr>
2018-06-28 19:53:06 +0200

    add refractor fragments

--- app/src/main/java/com/appdevloop/fragmentapp/fragments/DetailFragment.java
+++ app/src/main/java/com/appdevloop/fragmentapp/fragments/DetailFragment.java
@@ -1,25 +1,10 @@
 package com.appdevloop.fragmentapp.fragments;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.appdevloop.fragmentapp.R;
-
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import icepick.Icepick;
 import icepick.State;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,93 +13,39 @@ import static android.content.ContentValues.TAG;
  * Use the {@link DetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class DetailFragment extends BaseFragment {
 
-    // 1 - Declare a buttonTag tracking
-    @State int buttonTag;
-    // 2 - Create static variable to identify key in Bundle
-    private static final String KEY_BUTTONTAG = "DetailFragment.KEY_BUTTONTAG";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    // 1 - Adding @BindView in order to indicate to ButterKnife to get & serialise it
     @BindView(R.id.fragment_detail_text_view) TextView textView;
+    @State int buttonTag;
 
-    public DetailFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailFragment newInstance(String param1, String param2) {
-        DetailFragment fragment = new DetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // --------------
+    // BASE METHODS
+    // --------------
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    protected BaseFragment newInstance() { return new DetailFragment(); }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        //  Get textView from layout (don't forget to create ID in fragment_detail.xml)
-        //  Telling ButterKnife to bind all views in layout
-        ButterKnife.bind(this, view);
-        return(view);
-    }
+    protected int getFragmentLayout() { return R.layout.fragment_detail; }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        // Restore last buttonTag if possible
-        // Restore all @State annotation variables in Bundle
-        Icepick.restoreInstanceState(this, savedInstanceState);
+    protected void configureDesign() { }
+
+    @Override
+    protected void updateDesign() {
         this.updateTextView(this.buttonTag);
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //  Save buttonTag in Bundle when fragment is destroyed
-        //  Save all @State annotation variables in Bundle
-        Icepick.saveInstanceState(this, outState);
-    }
+    // --------------
+    // UPDATE UI
+    // --------------
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-    }
-
-    // 3 - Update TextView depending on TAG's button
+    //Update TextView depending on TAG's button
     public void updateTextView(int tag){
-        Log.i("DetailFragment", "updateTextView: " + tag);
-        // 3 - Save tag in ButtonTag variable
+
+        //Save tag in ButtonTag variable
         this.buttonTag = tag;
+
         switch (tag){
             case 10:
                 this.textView.setText("You're a very good programmer !");
@@ -129,5 +60,4 @@ public class DetailFragment extends Fragment {
                 break;
         }
     }
-
 }

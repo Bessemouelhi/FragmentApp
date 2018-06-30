58466923c904689387c7c74a7bc42faa18b4f0cd
bessemouelhi <bessem.mouelhi@outlook.fr>
2018-06-30 11:07:00 +0200

    add a TooBar

--- app/src/main/java/com/appdevloop/fragmentapp/Controllers/MainActivity.java
+++ app/src/main/java/com/appdevloop/fragmentapp/Controllers/MainActivity.java
@@ -3,7 +3,11 @@ package com.appdevloop.fragmentapp.Controllers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.appdevloop.fragmentapp.R;
 import com.appdevloop.fragmentapp.fragments.BlankFragment;
@@ -23,6 +27,39 @@ public class MainActivity extends AppCompatActivity implements BlankFragment.OnF
         this.configureAndShowMainFragment();
         // 2 - Configure and show detail fragment
         this.configureAndShowDetailFragment();
+
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //3 - Handle actions on menu items
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Il n'y a rien à paramétrer ici, passez votre chemin...", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Recherche indisponible, demandez plutôt l'avis de Google, c'est mieux et plus rapide.", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // ----
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
     private void configureAndShowMainFragment(){
